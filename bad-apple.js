32fc5f719a528c8d0a747cef2b56c56d47858d88
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-02-24 00:06:25 +0700

    testing func

--- bad-apple.js
+++ bad-apple.js
@@ -6,15 +6,22 @@ const player = require("play-sound")();
 const FILES = fs.readdirSync(FRAME_DIR).filter(file => file.endsWith(FILE_FORMAT)).sort();
 let iter = 0;
 
-const start = (width, height, alphabet) => {
-  const filePath = path.join(FRAME_DIR, FILES[iter]);
+const play = () => {
+  if (i >= files.length) i = 0;
 
-  ascii
-    .image({ filepath: filePath, width: width, height: height, alphabet: alphabet }).then((frame) => {
-      console.clear();
-      console.log(frame);
-      iter++;
-      setTimeout(start, FRAME_DELAY);
-    });
+  const filePath = path.join(FRAME_DIR, files[i]);
+
+  ascii.image({
+    filepath: filePath,
+    width: 120,
+    height: 40,
+    alphabet: "variant4"
+  }).then((frame) => {
+    console.clear();
+    console.log(frame);
+    i++;
+    setTimeout(play, FRAME_DELAY);
+  });
 };
 
+play();
