abb12fb28f119d25089f5ad186a1d364a0459ee5
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-02-24 00:05:53 +0700

    add start fun -v 1

--- bad-apple.js
+++ bad-apple.js
@@ -3,3 +3,18 @@ const path = require("path");
 const ascii = require("ascii-art");
 const player = require("play-sound")();
 
+const FILES = fs.readdirSync(FRAME_DIR).filter(file => file.endsWith(FILE_FORMAT)).sort();
+let iter = 0;
+
+const start = (width, height, alphabet) => {
+  const filePath = path.join(FRAME_DIR, FILES[iter]);
+
+  ascii
+    .image({ filepath: filePath, width: width, height: height, alphabet: alphabet }).then((frame) => {
+      console.clear();
+      console.log(frame);
+      iter++;
+      setTimeout(start, FRAME_DELAY);
+    });
+};
+
